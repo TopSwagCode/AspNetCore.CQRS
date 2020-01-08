823cc44bf52ca3c7cb573fab18e1ebcbf74508d1
Joshua Ryder <joshua.ryder@outlook.com>
2020-01-08 19:01:02 +0100

    Add dummy commands and queries

---
+++ TopSwagCode.WebApi.CQRS/Domain/Artifactory/Queries/GetAllFilesForReleaseFromArtifactoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopSwagCode.WebApi.CQRS.Domain.Artifactory.Queries
+{
+    public class GetAllFilesForReleaseFromArtifactoryQuery
+    {
+    }
+}
---
+++ TopSwagCode.WebApi.CQRS/Domain/User/Commands/UpdateUserAccessCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopSwagCode.WebApi.CQRS.Domain.User.Commands
+{
+    public class UpdateUserAccessCommand
+    {
+    }
+}
